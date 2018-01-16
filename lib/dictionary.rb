e5c0591f6e3103f51f9352f8ba93248bdcaa68ad
Brad Hambrice <hambrice@uab.edu>
2018-01-16 02:21:28 +0000

    built Question.select_word and select_options methods

--- lib/dictionary.rb
+++ lib/dictionary.rb
@@ -2,16 +2,20 @@ require "pry"
 require_relative "../lib/scraper.rb"
 
 class Dictionary
-  attr_reader :words
+ # attr_reader :words
   
   def initialize (url)
-    @words = Scraper.new.scrape_dictionary(url)
+    @@words = Scraper.new.scrape_dictionary(url)
   end
   
-  def define(word)
-    @words[word]
+  def self.define(word)
+    @@words[word]
   end
   
+  
+  def words
+    @@words
+  end
 end
 
 #dictionary = Dictionary.new("https://www.vocabulary.com/lists/1748998").define("luminous")
--- lib/question.rb
+++ lib/question.rb
@@ -2,17 +2,38 @@ require "pry"
 require_relative "../lib/dictionary.rb"
 
 class Question
- @@used_words
+ @@used_words = []
+ attr_reader :word, :answer
  
- def select_word(hash)
-   hash.keys
+  def select_word(hash)
+   array = hash.keys
+   number = rand(1..array.length+1)
+   while @@used_words.include? (array[number])
+     self.select_word(hash)
+   end
+   @@used_words << array[number]
+   @answer = Dictionary.define(array[number])
+   @word = array[number]
+ end
+ 
+ def select_options(hash)
+   new_array = []
+   self.select_word(hash)
+   array = hash.values.delete_if {|p| p == @answer}
+   #binding.pry
+   3.times do
+     number = rand(1..array.length+1)
+     new_array << array[number]
+     array[number].delete(array[number])
+   end
+     binding.pry
+     
+ end
+ 
+ def build_question(hash)
    
- end
- 
- def select_options
- end
- 
- def build_question
    
  end
 end
+
+Question.new.select_options(Dictionary.new("https://www.vocabulary.com/lists/1748998").words)

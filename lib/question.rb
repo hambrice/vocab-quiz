3aa5a6bf84343cd20f1dd0a7c251604be36b6146
Brad Hambrice <hambrice@uab.edu>
2018-01-16 03:53:45 +0000

    added :options attribute to Question

--- lib/question.rb
+++ lib/question.rb
@@ -3,6 +3,7 @@ require_relative "../lib/dictionary.rb"
 
 class Question
  @@used_words = []
+ attr_accessor :options
  attr_reader :word, :answer
  
   def select_word(hash)
@@ -16,27 +17,27 @@ class Question
    @word = array[number]
  end
 
- def options 
-   @options = []
- end
  
  def select_options(hash)
-   
+   self.options = []
    self.select_word(hash)
    array = hash.values.delete_if {|p| p == @answer}
    #binding.pry
    3.times do
      number = rand(1..array.length+1)
      self.options << array[number]
-     array[number].delete(array[number])
+     array.delete(array[number])
    end
   self.options
  end
  
  def build_question(hash)
    select_options(hash)
-   number = rand (0..self.options.length+1)
-   self.options
+   self.options << "placeholder"
+   number = rand (0..4)
+   self.options.insert(number, @answer)
+   self.options.delete("placeholder")
+   binding.pry
    puts "What is the definition of #{@word}?"
    
    
@@ -44,4 +45,4 @@ class Question
  end
 end
 
-Question.new.select_options(Dictionary.new("https://www.vocabulary.com/lists/1748998").words)
+Question.new.build_question(Dictionary.new("https://www.vocabulary.com/lists/1748998").words)

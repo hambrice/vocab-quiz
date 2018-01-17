127e16ec6c55e4b7179f2504c4844b71438e6962
Brad Hambrice <hambrice@uab.edu>
2018-01-17 07:45:37 +0000

    changed questions to have associated letter

--- lib/question.rb
+++ lib/question.rb
@@ -3,45 +3,52 @@ require_relative "../lib/dictionary.rb"
 
 class Question
  @@used_words = []
- attr_accessor :options, :answer_index
- attr_reader :word, :answer, :dictionary
+ attr_accessor :options, :answer_index, :answer, :word
  
-  def initialize(dictionary)
-    @dictionary = dictionary
-  end
   def self.used_words
     @@used_words
   end
   
+  def letter_to_index(input)
+    case input.downcase
+    when "a"
+      1
+    when "b"
+      2
+    when "c"
+      3
+    when "d"
+      4
+    end
+  end
+    
   def correct?(input)
+    if input.to_i == 0
+      self.correct?(self.letter_to_index(input))
+    end
+    #make this blank if blank later!
     input == self.answer_index + 1
   end
   
   def select_word(hash)
    array = hash.keys
    @@used_words.each {|w| array.delete_if {|s| s == w}}
-   #binding.pry
    number = rand(0..array.length-1)
-   if @@used_words.include? (array[number])
-     self.select_word(hash)
-   end
    @@used_words << array[number]
-   @answer = self.dictionary.define(array[number])
-   @word = array[number]
+   self.answer = hash[array[number]]
+   self.word = array[number]
  end
 
  
  def select_options(hash)
    self.options = []
    self.select_word(hash)
-   array = hash.values.delete_if {|p| p == @answer}
-   #binding.pry
+   array = hash.values.delete_if {|p| p == self.answer}
    3.times do
      number = rand(0..array.length-1)
      self.options << array[number]
      array.delete(array[number])
    end
-   #binding.pry
   self.options
  end
  
